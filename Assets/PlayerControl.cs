3c167b65bf7ab6935d06278a79d3e78ebd66b46d
JerryLin1 <jerreelynn@gmail.com>
2020-07-24 16:11:26 -0400

    fwef

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float movementSpeed = 3f;
+    float jumpSpeed = 500f;
+    bool isGrounded = true;
+
+    function Update()
     {
-        
+        if (isGrounded)
+        {
+            rigidbody2D.gravityScale = 2.1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            transform.rotation.y = 180; //This is for rotating sprite images
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+            transform.rotation.y = 0; //This is for rotating sprite images
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (!isGrounded)
+            {
+                rigidbody2D.gravityScale += 2;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (isGrounded)
+            {
+                rigidbody2D.AddForce(Vector3.up * jumpSpeed);
+                isGrounded = false;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    function OnCollisionEnter2D(col Collision2D)
     {
-        
+        isGrounded = true;
     }
 }
