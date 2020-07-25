3047764a83dd2fa641b281d051420d2545828d62
JerryLin1 <jerreelynn@gmail.com>
2020-07-25 17:00:20 -0400

    spawner

---
+++ Assets/SpawnerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerControl : MonoBehaviour
+{
+    public bool spawnsEnemies;
+    public bool spawnsPickups;
+    public List<GameObject> possibleEnemySpawns;
+    public List<GameObject> possiblePickupSpawns;
+    List<GameObject> totalSpawns = new List<GameObject>();
+
+    void Start()
+    {
+        totalSpawns.Add(new GameObject());
+        if (spawnsEnemies)
+        {
+            totalSpawns.AddRange(possibleEnemySpawns);
+        }
+        if (spawnsPickups)
+        {
+            totalSpawns.AddRange(possiblePickupSpawns);
+        }
+        if (totalSpawns.Count != 0) {
+            Instantiate(totalSpawns[Random.Range (0, totalSpawns.Count)], transform.position, Quaternion.identity);
+        }
+    }
+}
