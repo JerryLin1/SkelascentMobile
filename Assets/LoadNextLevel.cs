48e2e1514608f3cf3648414c84fb5cba5247a7b9
JerryLin1 <jerreelynn@gmail.com>
2020-07-26 16:45:25 -0400

    plzplz

---
+++ Assets/LoadNextLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public Animator transition; 
+
+    public void nextLevel() {
+        if (SceneManager.GetActiveScene().name == "LevelGeneration") {
+            StartCoroutine(fade("MainMenu"));
+        } else {
+            StartCoroutine(fade("LevelGeneration"));
+        }
+    }
+    public IEnumerator fade(string scene) {
+        transition.SetTrigger("start");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(scene);
+    }
+}
