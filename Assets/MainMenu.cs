417ee1ddc5779919ec70fc970cee221ee7638144
JerryLin1 <jerreelynn@gmail.com>
2020-07-26 16:38:16 -0400

    plz

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,13 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
     public void PlayGame() {
         SceneManager.LoadScene("LevelGeneration");
-    }
-
-    public void QuitGame() {
-        Debug.Log("quit game");
-        Application.Quit();
     }
 }
