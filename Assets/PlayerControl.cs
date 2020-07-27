8b82967638f34f0151035a5b607dcf4d0f97eb58
p-han-tom <hantom0127@gmail.com>
2020-07-26 23:36:44 -0400

    room fix again

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -12,7 +12,7 @@ public class PlayerControl : MonoBehaviour
     bool isGrounded;
     Transform feetPos;
     Transform boneSourcePos;
-    float checkRadius = 0.1f;
+    float checkRadius = 0.025f;
     public LayerMask groundLayer;
     public LayerMask enemyLayer;
     public GameObject bonePrefab;
