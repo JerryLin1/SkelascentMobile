1ecf43b236be4ba0e3f3fe7f422ff1baaa381ea7
JerryLin1 <jerreelynn@gmail.com>
2020-11-26 14:30:40 -0500

    purchases tab

---
+++ Assets/PurchasesTab.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PurchasesTab : MonoBehaviour
+{
+    Transform tab;
+    Transform purchasesScreen;
+    float offset = -365;
+    void Start()
+    {
+        tab = transform.Find("Tab");
+        purchasesScreen = transform.Find("Purchases");
+    }
+    public void TogglePurchases()
+    {
+        transform.DOLocalMoveX(transform.localPosition.x + offset, 0.5f);
+        offset *= -1;
+    }
+}
