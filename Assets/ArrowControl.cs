a699ea2ef5bd2bb2dfd71124abbed7fd1ab8c0d8
p-han-tom <hantom0127@gmail.com>
2020-11-26 12:39:22 -0500

    demon archer

---
+++ Assets/ArrowControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector2 position;
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(gameObject.layer, 8);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, 9);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, 11);
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(position, transform.position)> 7f) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.GetComponent<PlayerControl>().Die();
+            Destroy(gameObject);
+
+        }
+
+    }
+}
