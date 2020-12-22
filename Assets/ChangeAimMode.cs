c817c3574629ab2cea8fdb97c9c7712c62a5039a
JerryLin1 <jerreelynn@gmail.com>
2020-12-22 17:36:13 -0500

    menu settings

---
+++ Assets/ChangeAimMode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChangeAimMode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateText();
+    }
+    public void Toggle() {
+        if (PlayerPrefs.GetInt("usingAimJoystick", 0) == 0) {
+            PlayerPrefs.SetInt("usingAimJoystick", 1);
+        }
+        else {
+            PlayerPrefs.SetInt("usingAimJoystick", 0);
+        }
+        UpdateText();
+    }
+    void UpdateText() {
+        if (PlayerPrefs.GetInt("usingAimJoystick", 0) == 0) {
+            GetComponentInChildren<TextMeshProUGUI>().text = "Joystick";
+        }
+        else {
+            GetComponentInChildren<TextMeshProUGUI>().text = "Screen";
+        }
+    }
+}
